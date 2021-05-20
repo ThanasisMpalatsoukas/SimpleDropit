286e82a379f84a238c7e094a94c940f0dd2959df
IN_MOURITECH\nishantk <nishantk.in@mouritech.com>
2021-05-20 11:28:56 +0530

    Refactor: Unwanted files removed.

--- dev/index.js
+++
@@ -1,4 +0,0 @@
-import SimpleDropit from '../src/index';
-
-const simpleDropit1 = new SimpleDropit(document.getElementById('sdd-fileinput2'));
-const simpleDropit2 = new SimpleDropit(document.getElementById('sdd-fileinput1'));
