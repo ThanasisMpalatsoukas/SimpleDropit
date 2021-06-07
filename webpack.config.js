30ab6c69a1471ea42948ef144d301a66a3d371e0
IN_MOURITECH\nishantk <nishantk.in@mouritech.com>
2021-06-07 15:28:19 +0530

    add config for remote debugging and build.
    Demo folder is now the content of the website (on gh-pages branch)

--- webpack.config.js
+++ webpack.config.js
@@ -3,140 +3,101 @@ const webpack = require('webpack');
 const pkg = require('./package.json');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'production',
-    entry: './dev/index.js',
-    devtool: 'cheap-module-source-map',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './demo')
-    },
-    devServer: {
-        liveReload: true,
-        contentBase: [
-            path.resolve(__dirname, './dev'),
-        ],
-        publicPath: '/',
-        watchContentBase: true,
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                loader: "html-loader",
-                options: {
-                    minimize: false
-                }
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    "extract-loader",
-                    "css-loader",
-                ],
-            },
-        ],
-    },
-    plugins: [
+module.exports = (env, argv) => {
+    const devMode = argv.mode === 'development' ? true : false;
+
+    const plugins = [
         new HtmlWebpackPlugin({
-            template: './dev/index.html',
+            template: './website/index.html',
             filename: './index.html',
             minify: {
                 collapseWhitespace: false,
                 preserveLineBreaks: true
             }
-        })
-    ],
-    optimization: {
-        minimize: false
-    }
+        }),
+        new MiniCssExtractPlugin({
+            filename: devMode ? '[name].css' : '[name].[contenthash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[contenthash].css',
+        }),
+    ];
+
+    const config = [
+        {
+            mode: devMode ? 'development' : 'production',
+            entry: './website/index.js',
+            devtool: 'cheap-module-source-map',
+            output: {
+                filename: devMode ? 'bundle.js' : '[name].[contenthash].bundle.js',
+                path: path.resolve(__dirname, './demo')
+            },
+            devServer: {
+                liveReload: true,
+                contentBase: [
+                    path.resolve(__dirname, './../dist'),
+                ],
+                publicPath: '/',
+                watchContentBase: true,
+                compress: true,
+                port: 9000,
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.html$/,
+                        loader: "html-loader",
+                        options: {
+                            minimize: false
+                        }
+                    },
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        }
+                    },
+                    {
+                        test: /\.scss$/,
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            "css-loader",
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    postcssOptions: {
+                                        plugins: function () {
+                                            return [
+                                                require('autoprefixer')
+                                            ];
+                                        }
+                                    }
+                                }
+                            },
+                            "sass-loader",
+                        ]
+                    },
+                    {
+                        test: /\.css$/i,
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            "extract-loader",
+                            "css-loader",
+                            'postcs-loader'
+                        ]
+                    }
+                ],
+            },
+            plugins: plugins,
+            optimization: {
+                minimize: devMode ? false : true,
+            }
+        }
+    ];
+    
+    return config;
 };
-
-// const banner = [
-//     pkg.title + ' - ' + pkg.version,
-//     pkg.description,
-//     pkg.homepage,
-//     '',
-//     'Made by ' + pkg.author,
-//     'Under ' + pkg.license + ' License',
-//     '',
-// ].join('\n');
-
-// const devConfig = {
-//     mode: 'development',
-//     devtool: 'cheap-module-source-map',
-//     entry: './demo/index.js',
-//     output: {
-//         library: {
-//             name: 'SimpleDropit',
-//             type: 'umd',
-//             umdNamedDefine: true,
-//             export: 'default',
-//         },
-//         filename: 'simpledropit.js',
-//         path: path.resolve(__dirname, 'dist/js'),
-//         clean: false
-//     },
-//     devServer: {
-//         liveReload: true,
-//         contentBase: [
-//             path.resolve(__dirname, './src'),
-//             path.resolve(__dirname, './public'),
-//         ],
-//         watchContentBase: true,
-//         compress: true,
-//         port: 9000,
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.m?js$/,
-//                 exclude: /(node_modules|bower_components)/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         presets: ['@babel/preset-env']
-//                     }
-//                 }
-//             }
-//         ]
-//     },
-//     plugins: [
-//         new webpack.BannerPlugin(banner)
-//     ]
-// }
-
-// const prodConfig = {
-//     mode: 'production',
-//     entry: './src/index.js',
-//     output: {
-//         library: {
-//             name: 'SimpleDropit',
-//             type: 'umd2',
-//         },
-//         filename: 'simpledropit.min.js',
-//         path: path.resolve(__dirname, 'dist/js'),
-//         clean: false
-//     },
-//     plugins: [
-//         new webpack.BannerPlugin(banner)
-//     ]
-// };
-
-// module.exports = [
-//     devConfig,
-//     prodConfig
-// ];
